b2c9717dc75ff781e2a6c3054df9371bca7c0e49
OffworldAstronaut <rafael.science.amorim@gmail.com>
2024-07-02 11:37:29 -0300

    verif cpf completa

--- src/front-end/front-js/login-registro/validacao/funcoesDeValidacao/funcoesDeValidacao.js
+++ src/front-end/front-js/login-registro/validacao/funcoesDeValidacao/funcoesDeValidacao.js
@@ -33,7 +33,61 @@ export const passwordConfirmValidation = () => {
 
 1. pegue os digitos do cpf e multiplique de 10 a 2 em ordem decrescente
 2. multiplique o numero encontrado por 10, divida por 11 e armazene o resto da divisão (se ele for 10, transforme em 0)
-3. pegue os dígitos do cpf e o primeiro dígito
+3. pegue os dígitos do cpf e o primeiro dígito verificador e multiplicar na sequência decrescente de 11 até 12
+4. multiplique o numero encontrado por 10, divida por 11 e armazene o resto
 
 */
-export const cpfValidation = () => {};
+export const cpfValidation = (cpf) => {
+
+    // recebe o array em texto e quebra num array explícito para tratamento
+    let cpf_array = cpf.split("")
+    // elimina os caracteres que não são numéricos
+    cpf_array = cpf_array.filter(char => /^[0-9]+$/i.test(char))
+    // transforma todos os caracteres em números
+    cpf_array = cpf_array.map(char => Number(char));
+
+    // passo 1 do algoritmo
+
+    let prim_vef = 0
+    let num_mult = 10
+
+    cpf_array.forEach(element => {
+        prim_vef = prim_vef + element * num_mult
+        num_mult--
+    });
+
+    // passo 2 do algoritmo
+
+    prim_vef = (prim_vef * 10) % 11
+
+    if (prim_vef == 10){
+        prim_vef = 0
+    }
+
+    // passo 3 do algoritmo
+
+    let sec_vef = 0 
+    num_mult = 11
+
+    cpf_array.forEach(element => {
+        sec_vef = sec_vef + element * num_mult
+        num_mult--
+    });
+
+    // passo 4 do algoritmo 
+
+    sec_vef = (sec_vef * 10) % 11
+
+    if (prim_vef == cpf_array[-2] && sec_vef == cpf_array[-1]){
+        return true
+    }
+    else { 
+        return false
+    }
+
+
+
+
+};
+
+cpfValidation("123.456.789-00")
