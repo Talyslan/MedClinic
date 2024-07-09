12e77f02cc03d8289f823e372d0fe5d04b0c9339
OffworldAstronaut <rafael.science.amorim@gmail.com>
2024-07-09 08:28:29 -0300

    ajustes pessoa dao

--- src/back-end/banco-de-dados/DAO/superclasse/tableDAO.js
+++ src/back-end/banco-de-dados/DAO/superclasse/tableDAO.js
@@ -76,17 +76,15 @@ export class tableDAO {
 
   // em desenvolvimento
 
-  /* async updateOneOnDB(id, novosDados) {
+  async updateOneOnDB(id, novosDados) {
     const camposAtualizacao = Object.keys(novosDados).map(chave => `${chave} = ?`).join(", ");
     const valoresAtualizacao = Object.values(novosDados);
 
     // criação do update
     const sql_update = `
-      UPDATE ${this.constructor.sql_nomeTabela} 
+      UPDATE ${this.constructor.sql_nomeTabela}
       SET ${camposAtualizacao} 
-      WHERE id = ?;`;
-
-    valoresAtualizacao.push(id);
+      WHERE id = ${id};`;
 
     try {
       const result = await this._conexao.execute(sql_update, valoresAtualizacao);
@@ -99,5 +97,5 @@ export class tableDAO {
     }
   }
 
-  async deleteOneFromDB(id) {} */
+  // async deleteOneFromDB(id) {}
 }
